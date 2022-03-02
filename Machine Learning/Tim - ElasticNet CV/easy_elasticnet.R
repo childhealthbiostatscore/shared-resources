da0818a64ca3cc3d72ad3bb41f88e0b5b4799335
Tim Vigers <timothy.vigers@cuanschutz.edu>
2022-03-02 10:44:15 -0700

    Update easy_elasticnet.R

--- Machine Learning/Tim - ElasticNet CV/easy_elasticnet.R
+++ Machine Learning/Tim - ElasticNet CV/easy_elasticnet.R
@@ -7,11 +7,27 @@ load("/Volumes/PEDS/RI Biostatistics Core/Shared/Shared Projects/Laura/BDC/Proje
 # function in caret for additional details. There are two options for the output:
 # out = "min.error" produces the model with the lowest CV error, and 
 # out = "1se.error" produces all "acceptable" models (CV error within 
-# 1 standard error of the minimum).
-easy_glinternet = function(df,outcome,predictors,metric = "RMSE",
+# 1 standard error of the minimum). By default RMSE is the metric used for 
+# continuous variables, and accuracy for binary, but different metrics can be 
+# specified with the metric argument. Variables must be formatted correctly
+# (e.g. sex as a factor variable) for this to work properly! The only exception 
+# is that outcomes will automatically be converted to factor variables if binom = T.
+easy_glinternet = function(data,outcome,predictors,binom = F,metric = NULL,
                            cv_method = "LOOCV",folds = NULL,na = "na.omit",
                            out = "min.error"){
   require(caret)
+  require(glmnet)
+  df = data
+  # Make outcome binary if necessary
+  if(binom){
+    df[,outcome] = as.factor(df[,outcome])
+    if(is.null(metric)){
+      metric = "Accuracy"
+    }
+  }
+  if(is.null(metric)){
+    metric = "RMSE"
+  }
   # CV
   cv = trainControl(method = cv_method,number = folds)
   # Model formula
@@ -22,9 +38,38 @@ easy_glinternet = function(df,outcome,predictors,metric = "RMSE",
   # Train model
   t = train(f,data = df,method = "glmnet",na.action = na)
   # Get parameters for refitting
-  res = t$results
-  min_err = min(res[,metric],na.rm = T)
-  se_err = sd(res[,metric],na.rm = T)/sqrt(sum(!is.na(res[,metric])))
-  good_mods = which(res[,metric] <= (min_err + se_err))
-  params = res[good_mods,]
+  # Minimum error model
+  if (out == "min.error") {
+    params = t$bestTune
+  } else if (out == "1se.error") {
+    res = t$results
+    if(binom & metric == "Accuracy"){
+      res[,metric] = 1 - res[,metric]
+    }
+    min_err = min(res[,metric],na.rm = T)
+    se_err = sd(res[,metric],na.rm = T)/sqrt(sum(!is.na(res[,metric])))
+    good_mods = which(res[,metric] <= (min_err + se_err))
+    params = res[good_mods,]
+  }
+  # Refit models to get selected parameters (the coef() function output for caret is confusing)
+  # Matrices
+  X = model.matrix(update(f,.~.-1),df)
+  Y = df[rownames(X),outcome]
+  mods = apply(params,1,function(r){
+    a = as.numeric(r["alpha"])
+    l = as.numeric(r["lambda"])
+    if(binom){
+      mod = glmnet(y = Y,x = X,alpha = a,lambda = l,family = "binomial")
+    } else {
+      mod = glmnet(y = Y,x = X,alpha = a,lambda = l)
+    }
+    selected = as.matrix(coef(mod))
+    selected = rownames(selected)[selected[,1] != 0]
+    selected = selected[selected != "(Intercept)"]
+    # Rename
+    selected = predictors[match(selected,preds)]
+    return(selected)
+  })
+  names(mods) = NULL
+  return(mods)
 }
