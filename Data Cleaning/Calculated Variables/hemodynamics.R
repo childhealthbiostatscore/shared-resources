89c9842a0ddf56b31e46223d2993bb0453cd71e4
Tim VIgers <timothy.vigers@cuanschutz.edu>
2022-04-29 11:58:02 -0600

    hemodynamics done

--- Data Cleaning/Calculated Variables/hemodynamics.R
+++ Data Cleaning/Calculated Variables/hemodynamics.R
@@ -1,61 +1,6 @@
 # Function for calculating various hemodynamics for Petter Bjornstad's 
-# studies
-
-# SAS code from Petter:
-# data SUA.r01;
-# set SUA.r01;
-# 
-# 
-# 
-# TotProtgdL = total_protein;
-# HCt = mean (hematocrit_minus_5, hematocrit_90, hematocrit_120);
-# MAP_clamp = clamp_map;
-# HCt_clamp = HCt / 100;
-# 
-# 
-# 
-# * Absolute;
-# 
-# GFRabsmLs= GFR/60;
-# ERPFabsmLs= RPF/60;
-# FFabs= GFR/RPF;
-# RBFabs= RPF /(1-HCt_clamp);
-# RBFGomabs= (ERPFabsmLs/(1-HCt_clamp));
-# 
-# 
-# 
-# run;
-# 
-# 
-# 
-# data SUA.r01;
-# set SUA.r01;
-# 
-# 
-# 
-# RVRAbs= clamp_map/RBFabs ;
-# 
-# if group = 2 then Kfg=0.1012;
-# if group = 3 then Kfg=0.1733;
-# if group = 4 then Kfg=0.1733; *** for non-DM if DM=0 then Kfg=0.1733;*;
-# 
-# 
-# 
-# 
-# deltaPFabs= GFRabsmLs/Kfg;
-# 
-# Cm_abs= (TotProtgdL/FFabs)*log(1/(1-FFabs));
-# Pg_abs= 5*(Cm_abs-2);
-# Pglo_abs= Pg_abs+deltaPFabs+10;
-# Ra_abs= ((MAP_clamp-Pglo_abs)/RBFGomabs)*1328;
-# Re_abs= (GFRabsmLs/(Kfg*(RBFGomabs-GFRabsmLs)))*1328;
-# 
-# 
-# 
-# run;
-
-# In general studies with have either a minus 10 or minus 5 value for hematocrit,
-# but double check that it's okay to include both in the average if available.
+# studies. 
+# Groups are T1D, T2D, Obese Controls, Lean Lontrols.
 hemodynamics_calc = function(total_protein,gfr,rpf,map,
                              hematocrit_minus_10,hematocrit_minus_5,
                              hematocrit_90,hematocrit_120,group){
@@ -63,33 +8,34 @@ hemodynamics_calc = function(total_protein,gfr,rpf,map,
   TotProtgdL = total_protein
   HCt = rowMeans(cbind(hematocrit_minus_10,hematocrit_minus_5,
                        hematocrit_90,hematocrit_120),na.rm = T)
-  MAP_clamp = map;
-  HCt_clamp = HCt / 100;
+  HCt[is.nan(HCt)] = NA
+  MAP_clamp = map
+  HCt_clamp = HCt / 100
   # Calculate variables
-  GFRabsmLs= gfr/60;
-  ERPFabsmLs= rpf/60;
-  FFabs= gfr/rpf;
-  RBFabs= rpf /(1-HCt_clamp);
-  RBFGomabs= (ERPFabsmLs/(1-HCt_clamp));
-  RVRAbs = map/RBFabs ;
-
-  
-  if group = 2 then Kfg=0.1012;
-  if group = 3 then Kfg=0.1733;
-  if group = 4 then Kfg=0.1733; *** for non-DM if DM=0 then Kfg=0.1733;*;
-
-
-
-
-  deltaPFabs= GFRabsmLs/Kfg;
-
-  Cm_abs = (TotProtgdL/FFabs)*log(1/(1-FFabs));
-  Pg_abs = 5*(Cm_abs-2);
-  Pglo_abs = Pg_abs+deltaPFabs+10;
-  Ra_abs = ((MAP_clamp-Pglo_abs)/RBFGomabs)*1328;
-  Re_abs = (GFRabsmLs/(Kfg*(RBFGomabs-GFRabsmLs)))*1328;
-  
-  
+  GFRabsmLs = gfr/60
+  ERPFabsmLs = rpf/60
+  FFabs = gfr/rpf
+  RBFabs = rpf /(1-HCt_clamp)
+  RBFGomabs = (ERPFabsmLs/(1-HCt_clamp))
+  RVRAbs = map/RBFabs 
+  # Kfg by group
+  Kfg = NA
+  length(Kfg) = length(group)
+  Kfg[group %in% c("T1D","T2D")] = 0.1012
+  Kfg[group %in% c("Obese Control","Lean Control")] = 0.1733
+  # More calculations
+  deltaPFabs = GFRabsmLs/Kfg;
+  Cm_abs = (TotProtgdL/FFabs)*log(1/(1-FFabs))
+  Pg_abs = 5*(Cm_abs-2)
+  Pglo_abs = Pg_abs+deltaPFabs+10
+  Ra_abs = ((MAP_clamp-Pglo_abs)/RBFGomabs)*1328
+  Re_abs = (GFRabsmLs/(Kfg*(RBFGomabs-GFRabsmLs)))*1328
   # Return dataframe
-  return(data.frame(list()))
+  return(data.frame(list("GFRabsmLs" = GFRabsmLs,
+                         "ERPFabsmLs" = ERPFabsmLs,"FFabs" = FFabs,
+                         "RBFabs" = RBFabs,"RBFGomabs" = RBFGomabs,
+                         "RVRAbs" = RVRAbs,"deltaPFabs" = deltaPFabs,
+                         "Cm_abs" = Cm_abs,"Pg_abs" = Pg_abs,
+                         "Pglo_abs" = Pglo_abs,"Ra_abs" = Ra_abs,
+                         "Re_abs" = Re_abs)))
 }
